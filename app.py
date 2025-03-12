62d45c08cafea33e307ac45865ed7753c8472b4f
SaiyanSai <sents@farmingdale.edu>
2025-03-12 22:34:58 +0530

    XSS vulnerable code added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, render_template_string
 
 app = Flask(__name__)
 
@@ -9,6 +9,7 @@ PASSWORD = "P4$$W0RD"
 def home():
     return render_template('home.html')
 
+#This code is where the injection is intended to happen
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST' : 
@@ -21,5 +22,12 @@ def login():
             return 'Invalid Username or Password'
     return render_template('login.html')
 
+#This code is intentionally vulnerable to XSS
+@app.route('/greet', methods = ['GET'])
+def greet():
+    name = request.args.get('name')
+    response = f"<h3>H3LL0, {name} !!!! </h3>"
+    return render_template_string(response)
 if __name__ == '__main__':
     app.run(debug=True)
+    
